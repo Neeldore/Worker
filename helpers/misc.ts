7f762f1ad76a3865399272992a69f7934645baa0
NeelkantJain <neelkant@appveen.com>
2022-01-14 02:13:02 +0530

    Clean up -2

--- helpers/misc.ts
+++ helpers/misc.ts
@@ -1,3 +1,9 @@
+import { GO_BACK } from './constants';
+
 export function _throw(m = 'Generic error') {
   throw new Error(m);
+}
+
+export function checkOrEscape(ans, prop) {
+  return ans[prop] === GO_BACK.value ? _throw() : ans;
 }
--- services/odp/helpers/odphelper.ts
+++ services/odp/helpers/odphelper.ts
@@ -1,69 +1,34 @@
 import {
-  MY_DEFECTS,
-  MY_STORIES,
   DEFECT_DETAILS,
-  GO_BACK,
   GET_CLOUD_USERS,
   GET_SERVICE_DEF,
+  MY_DEFECTS,
+  MY_STORIES,
   STORY_DETAILS,
 } from '../../../helpers/constants';
-import { cloudGet, cloudPut } from './credManager';
 import {
   autoCompleteInquirer,
   basicInquirer,
   easyInquirer,
   odpStatusesInquirer,
 } from '../../../helpers/inquirer';
-import { _throw } from '../../../helpers/misc';
-
-export function _fetchDefect(skipCheck = false) {
-  return cloudGet(MY_DEFECTS, getDefaultFilter(), skipCheck);
-}
-
-export function _fetchStories(skipCheck = false) {
-  return cloudGet(MY_STORIES, getDefaultFilter(), skipCheck);
-}
-
-export function getDefectDetails(id, skipCheck = false) {
-  return cloudGet(`${DEFECT_DETAILS}/${id}`, { expand: true }, skipCheck);
-}
-export function getStoryDetails(id, skipCheck = false) {
-  return cloudGet(`${STORY_DETAILS}/${id}`, { expand: true }, skipCheck);
-}
+import { checkOrEscape, _throw } from '../../../helpers/misc';
+import { cloudGet, cloudPut } from './credManager';
 
 export function changeStatus(id: string) {
   let entityDetails: any;
-  let status = '';
   let isDefect = id.includes('DEF');
   const detailsFn = isDefect ? getDefectDetails : getStoryDetails;
+  const detailsApi = isDefect ? DEFECT_DETAILS : STORY_DETAILS;
+
   return detailsFn(id)
-    .then((resp) =>
-      resp && resp.data && resp.data._id
-        ? resp.data
-        : _throw('cannot find ' + isDefect ? 'Defect' : 'Story')
-    )
-    .then((resp) => {
-      entityDetails = resp;
-      return odpStatusesInquirer(isDefect);
-    })
-    .then((ans) => {
-      if (ans.status === GO_BACK.value) {
-        _throw();
-      }
-      status = ans.status;
-      entityDetails.status = status;
-      return cloudPut(
-        isDefect ? DEFECT_DETAILS : STORY_DETAILS,
-        id,
-        entityDetails,
-        true
-      );
-    })
-    .then((resp: any) =>
-      resp.status === 200
-        ? console.log(`\n Changed the status of ${id} to ${status} \n`)
-        : console.log('\n Some error occured \n' + JSON.stringify(resp.data))
-    )
+    .then((resp) => respDataFormatter(resp, isDefect))
+    .then((resp) => (entityDetails = resp))
+    .then(() => odpStatusesInquirer(isDefect))
+    .then((ans) => checkOrEscape(ans, 'status'))
+    .then((ans) => (entityDetails.status = ans.status))
+    .then(() => cloudPut(detailsApi, id, entityDetails, true))
+    .then((resp: any) => handleLogging(resp, id, entityDetails))
     .catch((e) => {});
 }
 
@@ -73,46 +38,22 @@ export function assignToQa(id) {
   let serviceDef: Array<any> = [];
   let isDefect = id.includes('DEF');
   const detailsFn = isDefect ? getDefectDetails : getStoryDetails;
+  const detailsApi = isDefect ? DEFECT_DETAILS : STORY_DETAILS;
 
-  return Promise.all([detailsFn(id), getServiceDefinition(isDefect)])
-    .then(([resp, _serviceDef]) => {
-      serviceDef = _serviceDef;
-      return resp && resp.data && resp.data._id
-        ? resp.data
-        : _throw('cannot find ' + isDefect ? 'Defect' : 'Story');
-    })
-    .then((resp) => {
-      entityDetails = resp;
-      nameOfQa = resp.nameOfTheQa || {};
-      return getUser(nameOfQa);
-    })
-    .then((ans) => {
-      nameOfQa = ans;
-      return getComponents(serviceDef);
-    })
-    .then((ans) => {
-      entityDetails.componentsAffected = ans.components;
-      entityDetails.status = isDefect ? 'Ready for QA' : 'Completed';
-      entityDetails.newEnvironmentVariables = 'No';
-      entityDetails.newErrorMessages = 'No';
-      entityDetails.newMigrationScripts = 'No';
-      entityDetails.newSchemaChanges = 'No';
-      entityDetails.newSetupScripts = 'No';
-      entityDetails.assignedTo = { _id: nameOfQa._id };
-      return cloudPut(
-        isDefect ? DEFECT_DETAILS : STORY_DETAILS,
-        id,
-        entityDetails,
-        true
-      );
-    })
-    .then((resp: any) =>
-      resp.status === 200
-        ? console.log(
-            `\n Changed the status of ${id} to ${entityDetails.status} \n`
-          )
-        : console.log('\n Some error occured \n' + JSON.stringify(resp.data))
-    )
+  return Promise.all([
+    detailsFn(id)
+      .then((resp) => respDataFormatter(resp, isDefect))
+      .then((resp) => (entityDetails = resp)),
+    getServiceDefinition(isDefect).then(
+      (_serviceDef) => (serviceDef = _serviceDef)
+    ),
+  ])
+    .then(([resp, _]) => (nameOfQa = resp.nameOfTheQa || {}))
+    .then(() => getUser(nameOfQa).then((ans) => (nameOfQa = ans)))
+    .then(() => getComponents(serviceDef).then((ans) => ans.components))
+    .then(() => setDefaultValues(entityDetails, isDefect, nameOfQa))
+    .then(() => cloudPut(detailsApi, id, entityDetails, true))
+    .then((resp: any) => handleLogging(resp, id, entityDetails))
     .catch((e) => {});
 }
 
@@ -133,14 +74,13 @@ export function getDefaultFilter() {
   };
 }
 
-export function formatChoices(response) {
-  return response.map((defect) => ({
+export const formatChoices = (response) =>
+  response.map((defect) => ({
     name: `${defect._id} - ${defect.summary} - ${defect.status} - ${
       defect.release.name || defect.release._id
     }`,
     value: defect._id,
   }));
-}
 
 export function fetchEntities(type) {
   const mapper = {
@@ -166,8 +106,45 @@ export function fetchEntities(type) {
     );
 }
 
-function userSource(_existinAnsers, name) {
-  return cloudGet(
+export const _fetchDefect = (skipCheck = false) =>
+  cloudGet(MY_DEFECTS, getDefaultFilter(), skipCheck);
+
+export const _fetchStories = (skipCheck = false) =>
+  cloudGet(MY_STORIES, getDefaultFilter(), skipCheck);
+
+export const getDefectDetails = (id, skipCheck = false) =>
+  cloudGet(`${DEFECT_DETAILS}/${id}`, { expand: true }, skipCheck);
+
+export const getStoryDetails = (id, skipCheck = false) =>
+  cloudGet(`${STORY_DETAILS}/${id}`, { expand: true }, skipCheck);
+
+const getServiceDefinition = (isDefect) =>
+  cloudGet(GET_SERVICE_DEF + '/' + (isDefect ? 'SRVC2022' : 'SRVC2020'), {
+    select: 'definition',
+  }).then((resp) => {
+    try {
+      return resp.data.definition.find(
+        (def) => def.key === 'componentsAffected'
+      ).definition[0].properties.enum;
+    } catch {
+      return [];
+    }
+  });
+
+const respDataFormatter = (resp, isDefect) =>
+  resp && resp.data && resp.data._id
+    ? resp.data
+    : _throw('cannot find ' + isDefect ? 'Defect' : 'Story');
+
+const handleLogging = (resp, id, entityDetails) =>
+  resp.status === 200
+    ? console.log(
+        `\n Changed the status of ${id} to ${entityDetails.status} \n`
+      )
+    : console.log('\n Some error occured \n' + JSON.stringify(resp.data));
+
+const userSource = (_existinAnsers, name) =>
+  cloudGet(
     GET_CLOUD_USERS,
     {
       count: 20,
@@ -181,24 +158,14 @@ function userSource(_existinAnsers, name) {
       value: { _id: users._id, name: users.name },
     }))
   );
-}
 
-function getServiceDefinition(isDefect) {
-  return cloudGet(
-    GET_SERVICE_DEF + '/' + (isDefect ? 'SRVC2022' : 'SRVC2020'),
-    {
-      select: 'definition',
-    }
-  ).then((resp) => {
-    try {
-      return resp.data.definition.find(
-        (def) => def.key === 'componentsAffected'
-      ).definition[0].properties.enum;
-    } catch {
-      return [];
-    }
+const getComponents = (serviceDef) =>
+  basicInquirer({
+    type: 'checkbox',
+    name: 'components',
+    message: 'selected the components affected',
+    choices: [{ name: 'just web', value: 'xcro_web' }, ...serviceDef],
   });
-}
 
 function getUser(nameOfQa) {
   if (nameOfQa._id) {
@@ -212,27 +179,21 @@ function getUser(nameOfQa) {
       ],
       'qa',
       'assign to selected QA or someone else ?'
-    ).then((ans) => {
-      if (ans.qa === 'SE') {
-        return autoCompleteInquirer(
-          'qa',
-          'Type the name of the user',
-          userSource
-        );
-      } else {
-        return ans;
-      }
-    });
+    ).then((ans) => (ans.qa === 'SE' ? userSourceWrapper() : ans));
   } else {
-    return autoCompleteInquirer('qa', 'Type the name of the user', userSource);
+    return userSourceWrapper();
   }
 }
 
-function getComponents(serviceDef) {
-  return basicInquirer({
-    type: 'checkbox',
-    name: 'components',
-    message: 'selected the components affected',
-    choices: [{ name: 'just web', value: 'xcro_web' }, ...serviceDef],
-  });
-}
+const userSourceWrapper = () =>
+  autoCompleteInquirer('qa', 'Type the name of the user', userSource);
+
+const setDefaultValues = (entityDetails, isDefect, nameOfQa) => {
+  entityDetails.status = isDefect ? 'Ready for QA' : 'Completed';
+  entityDetails.newEnvironmentVariables = 'No';
+  entityDetails.newErrorMessages = 'No';
+  entityDetails.newMigrationScripts = 'No';
+  entityDetails.newSchemaChanges = 'No';
+  entityDetails.newSetupScripts = 'No';
+  entityDetails.assignedTo = { _id: nameOfQa._id };
+};
